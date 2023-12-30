1426b2b72312a0c80f082e04cb39ec3ac410c470
Todd Spainhour <59629794+ToddSpainhour@users.noreply.github.com>
2023-12-29 21:22:50 -0600

    created data files for school and work history
    workHistoryData.js
    educationData.js

---
+++ data/educationData.js
@@ -0,0 +1,35 @@
+
+export const educationData = [
+
+    {
+        "degreeName": "Master of Science in Software Engineering",
+        "schoolName": "Kennesaw State University",
+        "yearsAttended": "2023-Present",
+        "schoolLogo": "",
+        "displayOnPage": true
+    },
+
+    {
+        "degreeName": "Foundations Certificate in Software Engineering",
+        "schoolName": "Kennesaw State University",
+        "yearsAttended": "2023-2023",
+        "schoolLogo": "",
+        "displayOnPage": true
+    },
+
+    {
+        "degreeName": "Web Development Certificate",
+        "schoolName": "Nashville Software School",
+        "yearsAttended": "2020-2021",
+        "schoolLogo": "",
+        "displayOnPage": true
+    },
+
+    {
+        "degreeName": "Bachelor of Fine Arts in Digital Filmmaking and Video Production",
+        "schoolName": "Art Institute of Atlanta",
+        "yearsAttended": "2003-2008",
+        "schoolLogo": "",
+        "displayOnPage": true
+    }
+]
---
+++ data/workHistoryData.js
@@ -0,0 +1,80 @@
+
+export const workHistoryData = [
+
+    {
+        "title": "Software Developer",
+        "organization": "Rutherford County Government",
+        "dateRange": "2021-Current",
+        "organizationLogo": "",
+        "jobDetails": [
+            "job responsibility 1", 
+            "job responsibility 2",
+            "job responsibility 3",
+            "job responsibility 4",
+            "job responsibility 5",
+            "job responsibility 6",
+            "job responsibility 7",
+            "job responsibility 8",
+        ],
+        "makeVisiableOnSite": true
+    },
+
+    {
+        "title": "Creative Media Producer",
+        "organization": "Tennessee Department of State",
+        "dateRange": "2015-2021",
+        "organizationLogo": "",
+        "jobDetails": [
+            "job responsibility 1", 
+            "job responsibility 2",
+            "job responsibility 3",
+            "job responsibility 4",
+            "job responsibility 5",
+            "job responsibility 6",
+        ],
+        "makeVisiableOnSite": true
+    },
+
+    {
+        "title": "Digital Imaging Specialist",
+        "organization": "Tennessee State Library and Archives",
+        "dateRange": "2011-2015",
+        "organizationLogo": "",
+        "jobDetails": [
+            "job responsibility 1", 
+            "job responsibility 2",
+            "job responsibility 3",
+            "job responsibility 4",
+            "job responsibility 5",
+        ],
+        "makeVisiableOnSite": true
+    },
+
+    {
+        "title": "News Photojournalist",
+        "organization": "News 12 WRDW-TV",
+        "dateRange": "2008-2010",
+        "organizationLogo": "",
+        "jobDetails": [
+            "job responsibility 1", 
+            "job responsibility 2",
+            "job responsibility 3",
+            "job responsibility 4",
+        ],
+        "makeVisiableOnSite": true
+    },
+
+    {
+        "title": "31R - Multichannel Transmission Systems Operator",
+        "organization": "United States Army",
+        "dateRange": "1999-2003",
+        "organizationLogo": "",
+        "jobDetails": [
+            "job responsibility 1", 
+            "job responsibility 2",
+            "job responsibility 3",
+
+        ],
+        "makeVisiableOnSite": true
+    }
+]
